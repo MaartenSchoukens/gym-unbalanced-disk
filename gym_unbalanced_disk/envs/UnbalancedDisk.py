ea7c0ebc5eb41e86afb8e426470a0469a8f9dcc7
Gé van Otterdijk <gevanotterdijk@gmail.com>
2025-05-21 11:04:33 +0200

    Fix the variables for the simulation example

--- gym_unbalanced_disk/envs/UnbalancedDisk.py
+++ gym_unbalanced_disk/envs/UnbalancedDisk.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
         for i in range(100):
             time.sleep(1/24)
             u = 3#env.action_space.sample()
-            obs, reward, done, info = env.step(u)
+            obs, reward, terminated, truncated, info = env.step(u)
             Y.append(obs)
             env.render()
     finally:
